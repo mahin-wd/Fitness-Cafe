4800c0764c7b9ac4ab289cfabee75cbfd38b4e6d
mahin-wd <wdmahin.mhm@gmail.com>
2022-09-28 22:33:45 +0600

    body part finished

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Exercise from '../Exercise/Exercise';
 import Head from '../Head/Head';
 import './Body.css'
 
 const Body = () => {
+    const [exercises, setExercises] = useState([]);
+
+    useEffect( () => {
+        fetch('exercise.json')
+        .then(res => res.json())
+        .then(data => setExercises(data))
+    }, [])
+
     return (
         <div className='body'>
             <Head></Head>
             <div className='main-body'>
                 <div className="exercises">
-                    <h2>exercises here</h2>
+                    {
+                        exercises.map(exercise => <Exercise 
+                                                        key={exercise.id}
+                                                        exercise={exercise}
+                                                        ></Exercise>)
+                    }
                 </div>
 
                 <div className="count-exercise">
---
+++ src/Components/Exercise/Exercise.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Exercise.css'
+
+const Exercise = (props) => {
+    const {name, img, time, text, age} = props.exercise; 
+    return (
+        <div className='card'>
+            <div>
+                <img src={img} alt="" />
+                <h2>{name}</h2>
+                <p>{text}</p>
+                <p>{age}</p>
+                <p>Time: {time}</p>
+                <button>Add to list</button>
+            </div>
+        </div>
+    );
+};
+
+export default Exercise;
