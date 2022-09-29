c0d23920c9c2c07c055b33941d065100976837d8
mahin-wd <wdmahin.mhm@gmail.com>
2022-09-29 11:09:36 +0600

    first question

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Calculation from '../../Calculation/Calculation';
+import Calculation from '../Calculation/Calculation';
 import Exercise from '../Exercise/Exercise';
 import Head from '../Head/Head';
 import './Body.css'
@@ -7,6 +7,8 @@ import './Body.css'
 const Body = () => {
     const [exercises, setExercises] = useState([]);
     const [time, setTime] = useState([]);
+    const [breakTimes, setBreakTimes] = useState([]);
+    const [takeBreak, setTakeBreak] = useState([])
 
     useEffect( () => {
         fetch('exercise.json')
@@ -14,12 +16,22 @@ const Body = () => {
         .then(data => setExercises(data))
     }, [])
 
-    const addTime = (exercises) =>{
-        // console.log(exercises.time)
-        const netTime = [...time, exercises];
-        setTime(netTime);
-        // console.log(time);
+    useEffect( () => {
+        fetch('break.json')
+        .then(res => res.json())
+        .then(data => setBreakTimes(data));
+    }, [])
+
+    const addTime = (exercises) => {
+
+        const newTime = [...time, exercises];
+        setTime(newTime);
         
+    }
+
+    const addBreak = () => {
+        breakTimes.map(breakTime => setTakeBreak(breakTime))
+        console.log(takeBreak);
     }
 
     return (
@@ -34,10 +46,16 @@ const Body = () => {
                                                         addTime={addTime}
                                                         ></Exercise>)
                     }
+                    <div className='question'>
+                        <h2> 1) How does react work?</h2>
+                        <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+                    </div>
                 </div>
 
                 <div className="count-exercise">
-                    <Calculation time={time}></Calculation>
+                    <Calculation time={time}
+                                breakTimes={breakTimes}
+                                addBreak={addBreak} ></Calculation>
                 </div>
             </div>
         </div>
--- src/Components/Calculation/Calculation.js
+++ src/Components/Calculation/Calculation.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import './image/user.jpg'
 import './Calculation.css'
 const Calculation = (props) => {
     const {time} = props;
-    // console.log(times)
+    const {breakTimes} = props;
 
     let totalTime = 0;
     for (const times of time){
         totalTime = totalTime + times.time;
     }
+
+
+    let totalBreak = 0;
+    for (const breakTime in breakTimes) {
+        // console.log(breakTime);
+        totalBreak = totalBreak + breakTime;
+    }
+
+    // let totalBreak = 0;
+    // for (const addBreaks of addBreak){
+    //     totalBreak = totalBreak + addBreaks.break
+    // }
     return (
         <div className='calc'>
             <div className="user-profile">
-                <img src="image/user.jpg" alt="" />
+                
                 <h3>Mahin</h3>
             </div>
 
@@ -35,17 +46,16 @@ const Calculation = (props) => {
             <h2>Add a break</h2>
 
             <div className="add-break">
-                <button>10s</button>
-                <button>20s</button>
-                <button>30s</button>
-                <button>40s</button>
-                <button>50s</button>
+                {
+                    breakTimes.map(breakTime => <button onClick={() => {props.addBreak(props.exercise)}}>{breakTime.break}s</button>)
+                }
+                
             </div>
 
             <h3>Exercise Detail</h3>
             <div className='exercise-detail'>
                 <h4>Exercise Time: {totalTime}s</h4>
-                <h4>Break Tike: {}</h4>
+                <h4>Break Time: {0}s</h4>
             </div>
             <button className='activity-finished'>Activity Completed</button>
         </div>
